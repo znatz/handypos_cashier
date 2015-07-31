422228274bb6a6a84390e010aff50f77794e63d2
znatz <z.nat.cn@gmail.com>
2015-07-31 10:01:40 +0900

    first commit Xcode 7.0 iOS 6.1
    Library APNumberPad

--- HandyPOS_Cashier/Home.h
+++ HandyPOS_Cashier/Home.h
@@ -7,8 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import <APNumberPad/APNumberPad.h>
+@interface Home : UIViewController
 
 
 @end
---
+++ HandyPOS_Cashier/Home.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  HandyPOS_Cashier
+//
+//  Created by ZNAZT on 2015/07/31.
+//  Copyright © 2015年 ZNATZ . All rights reserved.
+//
+
+#import "Home.h"
+
+@interface Home () <APNumberPadDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *receiptNO;
+@property (weak, nonatomic) IBOutlet UITextField *tableNO;
+@end
+
+@implementation Home
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _receiptNO.inputView = ({
+        APNumberPad *numberPad = [APNumberPad numberPadWithDelegate:self];
+        [numberPad.leftFunctionButton setTitle:@"検索" forState:UIControlStateNormal];
+        numberPad.leftFunctionButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+        numberPad.leftFunctionButton.tag = 0;
+        numberPad;
+    });
+    
+    _tableNO.inputView = ({
+        APNumberPad *numberPad = [APNumberPad numberPadWithDelegate:self];
+        [numberPad.leftFunctionButton setTitle:@"検索" forState:UIControlStateNormal];
+        numberPad.leftFunctionButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+        numberPad.leftFunctionButton.tag = 1;
+        numberPad;
+    });
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+ #pragma mark - APNumberPadDelegate
+ 
+ - (void)numberPad:(APNumberPad *)numberPad functionButtonAction:(UIButton *)functionButton textInput:(UIResponder<UITextInput> *)textInput {
+     [textInput insertText:[NSString stringWithFormat:@"%ld", (long)functionButton.tag]];}
+@end
